c7570794b6a5b143cdc0871db67331b4af49da40
Omer Szerer <omerszerer@gmail.com>
2024-09-14 11:49:16 +0200

    Add color control

--- src/ColorContainer.jsx
+++ src/ColorContainer.jsx
@@ -1,10 +1,14 @@
 import randomColor from 'randomcolor';
 import React, { useState } from 'react';
 
-export default function ColorContainer() {
-  const color = randomColor();
-
-  console.log(color);
+export default function RandomColorControl() {
+  const [color, setColor] = useState(randomColor);
+  const [luminosity, setLuminosity] = useState('');
+  const [hue, setHue] = useState('');
+  const parameters = {
+    luminosity: luminosity,
+    hue: hue,
+  };
 
   return (
     <div className="main-container">
@@ -14,13 +18,26 @@ export default function ColorContainer() {
         <p>Generated Color: {color}</p>
       </div>
 
-      <button
-        onClick={() => {
-          const newColor = randomColor();
-        }}
-      >
+      <button onClick={() => setColor(randomColor(parameters))}>
         Generate
       </button>
+
+      <div className="inputs">
+        <h>Want more control?</h>
+        <input
+          value={luminosity}
+          onChange={(event) => setLuminosity(event.target.value)}
+          placeholder="light / dark"
+          className="luminosity-input"
+        />
+
+        <input
+          value={hue}
+          onChange={(event) => setHue(event.target.value)}
+          placeholder="red, green, blue etc."
+          className="hue-input"
+        />
+      </div>
     </div>
   );
 }
